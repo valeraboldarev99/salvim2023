d789a5954bf836d8d75eb887d4016ad22e5fcc13
Valera Boldarev <vboldarev@weltkind.com>
2023-02-01 10:39:29 +0600

    update company slider

--- js/core.js
+++ js/core.js
@@ -107,42 +107,42 @@ $(document).ready(function () {
 	});
 
 	$('.slider__digitals_content').slick({
-	    slidesToShow: 1,
-	    slidesToScroll: 1,
-	    arrows: false,
-	    dots:false,
-	    draggable:false,
-	    // autoplay: true,
-	    fade: true,
-	    asNavFor: '.slider__digitals'
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		dots:false,
+		draggable:false,
+		// autoplay: true,
+		fade: true,
+		asNavFor: '.slider__digitals'
 	});
 
 	// cookie asker
 	function setCookie(name, value, days) {
-	    let expires = "";
-	    if (days) {
-	        let date = new Date();
-	        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-	        expires = "; expires=" + date.toUTCString();
-	    }
-	    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+		let expires = "";
+		if (days) {
+			let date = new Date();
+			date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+			expires = "; expires=" + date.toUTCString();
+		}
+		document.cookie = name + "=" + (value || "") + expires + "; path=/";
 	}
 	function getCookie(name) {
-	    let matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
-	    return matches ? decodeURIComponent(matches[1]) : undefined;
+		let matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
+		return matches ? decodeURIComponent(matches[1]) : undefined;
 	}
 	function checkCookies() {
-	    let cookieNote = document.getElementById('cookie__note'); //окно cookie
-	    let cookieBtnAccept = cookieNote.querySelector('.cookie__accept'); //кнопка согласия
-	    // Если куки cookies_policy нет или она просрочена, то показываем уведомление
-	    if (!getCookie('cookies_policy')) {
-	        cookieNote.classList.add('show'); //класс для показа/скрытия окна куки
-	    }
-	    // При клике на кнопку устанавливаем куку cookies_policy на один год
-	    cookieBtnAccept.addEventListener('click', function () {
-	        setCookie('cookies_policy', 'true', 365);
-	        cookieNote.classList.remove('show'); //класс для показа/скрытия окна куки
-	    });
+		let cookieNote = document.getElementById('cookie__note'); //окно cookie
+		let cookieBtnAccept = cookieNote.querySelector('.cookie__accept'); //кнопка согласия
+		// Если куки cookies_policy нет или она просрочена, то показываем уведомление
+		if (!getCookie('cookies_policy')) {
+			cookieNote.classList.add('show'); //класс для показа/скрытия окна куки
+		}
+		// При клике на кнопку устанавливаем куку cookies_policy на один год
+		cookieBtnAccept.addEventListener('click', function () {
+			setCookie('cookies_policy', 'true', 365);
+			cookieNote.classList.remove('show'); //класс для показа/скрытия окна куки
+		});
 	}
 	checkCookies();
 	// end cookie asker
